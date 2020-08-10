9a6b38d7eff11c04178a5e81e6e70a82ffe83b80
Guillaume R <guillaume.rondon@gmail.com>
2020-08-10 17:04:22 +0200

    Linting, refactoring

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,75 +5,87 @@ from abc import ABC, abstractmethod
 class Strategy(ABC):
 
     @abstractmethod
-    def updateItem(self, item):
+    def update_item(self, item):
         pass
 
-    def expire(self, item):
+    @staticmethod
+    def expire(item):
         item.quality = 0
-                
-    def decreaseSellinByOne(self, item):
+
+    @staticmethod
+    def decrease_sellin_by_one(item):
         item.sell_in = item.sell_in - 1
-    
-    def qualityNotMaxed(self, item):
+
+    @staticmethod
+    def qualityNotMaxed(item):
         return item.quality < 50
 
-    def stillHasQuality(self, item):
+    @staticmethod
+    def stillHasQuality(item):
         return item.quality > 0
-               
-    def afterSellin(self, item):           
+
+    @staticmethod
+    def afterSellin(item):
         return item.sell_in < 0
 
-    def decreaseQualityByNbUntilZero(self, item, nb):
+    def decrease_quality_by_nb_until_zero(self, item, nb):
         for _i in range(nb):
             if self.stillHasQuality(item):
                 item.quality = item.quality - 1
 
-    def increaseQualityByOneUntilMax(self, item):
+    def increase_quality_by_one_until_max(self, item):
         if self.qualityNotMaxed(item):
             item.quality = item.quality + 1
 
+
 class ConcreteStrategyNormalItem(Strategy):
-    def updateItem(self, item):
-        self.decreaseQualityByNbUntilZero(item, 1)
-        self.decreaseSellinByOne(item)
+    def update_item(self, item):
+        self.decrease_quality_by_nb_until_zero(item, 1)
+        self.decrease_sellin_by_one(item)
         if self.afterSellin(item):
-            self.decreaseQualityByNbUntilZero(item, 1) 
+            self.decrease_quality_by_nb_until_zero(item, 1)
+
 
 class ConcreteStrategyLegendaryItem(Strategy):
-    def updateItem(self, item):
+    def update_item(self, item):
         pass
 
+
 class ConcreteStrategyConjuredItem(Strategy):
-    def updateItem(self, item):
-        self.decreaseQualityByNbUntilZero(item, 2)
-        self.decreaseSellinByOne(item)
+    def update_item(self, item):
+        self.decrease_quality_by_nb_until_zero(item, 2)
+        self.decrease_sellin_by_one(item)
         if self.afterSellin(item):
-            self.decreaseQualityByNbUntilZero(item, 2) 
+            self.decrease_quality_by_nb_until_zero(item, 2)
+
 
 class ConcreteStrategyImprovingUntilExpiration(Strategy):
-    def updateItem(self, item):
-        self.increaseQualityByOneUntilMax(item)
+    def update_item(self, item):
+        self.increase_quality_by_one_until_max(item)
         if item.sell_in < 11:
-            self.increaseQualityByOneUntilMax(item)
+            self.increase_quality_by_one_until_max(item)
         if item.sell_in < 6:
-            self.increaseQualityByOneUntilMax(item)
-        self.decreaseSellinByOne(item)
+            self.increase_quality_by_one_until_max(item)
+        self.decrease_sellin_by_one(item)
         if self.afterSellin(item):
             self.expire(item)
 
+
 class ConcreteStrategyImprovingItem(Strategy):
-    def updateItem(self, item):
-        self.increaseQualityByOneUntilMax(item)
-        self.decreaseSellinByOne(item)
+    def update_item(self, item):
+        self.increase_quality_by_one_until_max(item)
+        self.decrease_sellin_by_one(item)
         if self.afterSellin(item):
-            self.increaseQualityByOneUntilMax(item)
+            self.increase_quality_by_one_until_max(item)
+
 
 class Context():
     def __init__(self, strategy: Strategy) -> None:
         self._strategy = strategy
 
-    def updateItem(self, item) -> None:
-        self._strategy.updateItem(item)
+    def update_item(self, item) -> None:
+        self._strategy.update_item(item)
+
 
 class GildedRose(object):
 
@@ -82,33 +94,38 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            self.updateItem(item)
+            self.update_item(item)
 
-    def updateItem(self, item):
-        if self.isImprovingOverTime(item):
+    def update_item(self, item):
+        if self.is_improving_over_time(item):
             context = Context(ConcreteStrategyImprovingItem())
-        elif self.isImprovingUntilEvent(item):
+        elif self.is_improving_until_event(item):
             context = Context(ConcreteStrategyImprovingUntilExpiration())
-        elif self.isLegendary(item):
+        elif self.is_legendary(item):
             context = Context(ConcreteStrategyLegendaryItem())
-        elif self.isConjured(item):
+        elif self.is_conjured(item):
             context = Context(ConcreteStrategyConjuredItem())
         else:
             context = Context(ConcreteStrategyNormalItem())
-            
-        context.updateItem(item)
 
-    def isLegendary(self, item):
+        context.update_item(item)
+
+    @staticmethod
+    def is_legendary(item):
         return item.name in ["Sulfuras, Hand of Ragnaros"]
 
-    def isConjured(self, item):
+    @staticmethod
+    def is_conjured(item):
         return item.name.startswith("Conjured")
 
-    def isImprovingUntilEvent(self, item):
+    @staticmethod
+    def is_improving_until_event(item):
         return item.name.startswith("Backstage passes")
 
-    def isImprovingOverTime(self, item):
+    @staticmethod
+    def is_improving_over_time(item):
         return item.name in ["Aged Brie"]
+
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -118,5 +135,3 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-
